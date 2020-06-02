a9b602c331c472602e272e9e8ad13718993fa9d2
DianaNOSE <cc.esthefania@gmail.com>
2020-06-02 14:19:22 -0600

    aimportando Torch para usar la linterna

--- App.js
+++ App.js
@@ -2,46 +2,85 @@
  * Autor: Diana Esthefania Carrillo Cano
  */
 
-import React from 'react';
-import {  StyleSheet,  View,  Text, Button, Vibration} from 'react-native';
+import React, { Component } from 'react';
+import {  StyleSheet,  View,  Text, Button, Vibration, PermissionsAndroid, TouchableOpacity} from 'react-native';
 import SwipeButton from 'rn-swipe-button';
 import Toast from 'react-native-simple-toast';
+import Torch from 'react-native-torch';
 
-export default function App() {
 
-  const DURATION = 1500 ;
-  const NOTIF = 200 ;
-  let VIBENABLED = 0;
-  let VIBRATING = 0;
-  let StartVibrationFunction
-  let StopVibrationFunction
-  let NotificationOfVibrationEnabled
+export async function linterna() {
+  
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.CAMERA,
+      {
+        'title': 'Permisos de Camara - ReactNativeCode',
+        'message': 'La aplicación de ReactNativeCode necesita acceso a tu Camara.'
+      }
+    )
 
-  NotificationOfVibrationEnabled=()=>{
-    Vibration.vibrate(NOTIF);
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      Alert.alert("Permisos aceptados");
+    }
+    else {
+      Alert.alert("Permisos denegados");
+    }
+  } catch (err) {
+    console.warn(err)
   }
 
-  StartVibrationFunction=()=>{
-    if (VIBENABLED==1){
-      if (VIBRATING==0){
-        // Device Will Vibrate for 10 seconds
-        Vibration.vibrate(DURATION) ;
-        VIBRATING=1;
-      }
-    }}
+}//fin function Linterna()
 
-  StopVibrationFunction=()=>{
-    if(VIBRATING==1){
-      Vibration.cancel();
-      VIBRATING=0;
-    }
-   }
 
-  return (
-    <View style={styles.container}>
+  
+export default class App extends Component {
+
+  async componentDidMount() {
+    await linterna()
+  }
+
+  turnONTorch() {
+    Torch.switchState(true); // Encender Torch.
+  }
+
+  turnOFFTorch() {
+    Torch.switchState(false); // Apagar Torch.
+  }
+
+  
+  render() {
+    return (
+      <View style={styles.container} >
         <Text style={styles.paragraph}>
           WIMP
         </Text>
+
+        <TouchableOpacity onPress={this.turnONTorch.bind(this)} activeOpacity={0.6}>
+          <Text> Encender linterna </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={this.turnOFFTorch.bind(this)} activeOpacity={0.6} >
+          <Text> Apagar linterna </Text>
+        </TouchableOpacity>
+
+        <SwipeButton      //LINTERNA
+            disabled={false}
+            railBackgroundColor="white" //fondo
+            railBorderColor="grey"    //borde de switch
+            railFillBorderColor="pink"  //borde de deslizamiento 
+            railFillBackgroundColor="168, 64, 239, 0.38" //fondo deslizamiento
+            swipeSuccessThreshold= "55"//55% de deslizamiento para cambiar de estado
+            title="Linterna"
+            onSwipeSuccess={() => {     //cuando el swipebutton cambie de estado ON 
+              Toast.showWithGravity("Linterna activada :D", Toast.LONG, Toast.CENTER);                             
+              Torch.switchState(true);
+            }}
+            onSwipeFail={() => {      //cuando el swipebutton cambie de estado OFF 
+              Toast.showWithGravity("Linterna desactivada D:", Toast.LONG, Toast.CENTER);
+              Torch.switchState(false);
+            }}
+        />
 
         <SwipeButton      //VIBRACIÓN
             disabled={false}
@@ -51,23 +90,27 @@ export default function App() {
             railFillBackgroundColor="168, 64, 239, 0.38" //fondo deslizamiento
             swipeSuccessThreshold= "55"//55% de deslizamiento para cambiar de estado
             title="Vibración"
-            onSwipeSuccess={() => {     //cuando cambie de estado ON muestra un toast
-              Toast.showWithGravity("Encendido", Toast.LONG, Toast.CENTER);
-              NotificationOfVibrationEnabled();
-                                    VIBENABLED=1;
+            onSwipeSuccess={() => {     //cuando el swipebutton cambie de estado ON
+              Toast.showWithGravity(" Vibración activada :D", Toast.LONG, Toast.CENTER);
+              Vibration.vibrate(300);
+              
             }}
-            onSwipeFail={() => {      //cuando cambie de estado OFF muestra un toast
-              Toast.showWithGravity("Apagado", Toast.LONG, Toast.CENTER);
-              VIBENABLED=0;
+            onSwipeFail={() => {      //cuando el swipebutton cambie de estado OFF
+              Toast.showWithGravity("Vibración desactivada D:", Toast.LONG, Toast.CENTER);
+              Vibration.cancel();
             }}
         />
 
     </View>
     
-  );
-}
+    );//fin return
+  }//fin render
+}//Fin class App
 
-const styles = StyleSheet.create({
+
+
+styles = StyleSheet.create(
+{
   container: {
     flex: 1,
     justifyContent: 'center',
@@ -79,5 +122,8 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: 'bold',
     textAlign: 'center',
-  },
+  }, TextStyle: {
+    color: '#fff',
+    textAlign: 'center',
+  }
 });
--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # WhereIsMyPhone
 
 # PRIMEROS PASOS
-# Crear el repositorio en GitHub
-# Instalar node.js y git
-# En la terminal de Git correr los comandos: git clone (link del repositorio) ->  react-native init wimp -> cd wimp/ -> npx react-native run-android
+Crear el repositorio en GitHub
+Instalar node.js y git
+En la terminal de Git correr los comandos: git clone (link del repositorio) ->  react-native init wimp -> cd wimp/ -> npx react-native run-android
 
-# Instalar el swipe Button: npm i rn-swipe-button
-# Instalar el Toast: npm i react-native-simple-toast
+Instalar el swipe Button: npm i rn-swipe-button
+Instalar el Toast: npm i react-native-simple-toast
+Instalar Torch: npm i react-native-torch ( iOS (>= 8.0) y Android (todas las versiones) )
